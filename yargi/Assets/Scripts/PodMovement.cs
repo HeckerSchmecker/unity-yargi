3deb4a4e5fe3b8f1667ea76450b201f34d914202
HeckerSchmecker <glorious.gorim@gmail.com>
2023-04-20 18:00:30 +0200

    Basic Movement in x-direction finished
    Rotation around x-Axis based on x-Velocity. x-Pos clamps

--- yargi/Assets/Scripts/PodMovement.cs
+++ yargi/Assets/Scripts/PodMovement.cs
@@ -4,50 +4,75 @@ using UnityEngine;
 
 public class PodMovement : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    float torque = 0.05f;
-    float moveSpeed = 5f;
-    float rotationSpeed = 50f;
-    private float horizontal;
-    private float maxRotation = 0.3f;
-    
+    float rotationSpeed = 100f;
 
-    
+    private float movementSpeed = 4f;
+    private float maxDistance = 14f;
+    private float maxRotation = 30;
+
+    private Rigidbody rb;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        
+        
+
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(transform.rotation);
-        if (Input.GetKey(KeyCode.A) && transform.rotation.z < maxRotation)
-        {
-            transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D) && transform.rotation.z > -maxRotation)
-        {
-            transform.Rotate(new Vector3(0, 0, 1) * -rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            if (transform.rotation.z > 0.001)
-            {
-                transform.Rotate(new Vector3(0, 0, 1) * -rotationSpeed * Time.deltaTime);
-            }
-            else if(transform.rotation.z < 0.001)
-            {
-                transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
-            }
-        }
+        
+        //----------------Set x-Velocity ----------------
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 velocity = rb.velocity;
+        velocity.x = horizontalInput * movementSpeed;
+        rb.velocity = velocity;
+
+        
+
+        //-------------------Clamp X-Distance ----------------
+
+        // Get the current position of the object
+        Vector3 currentPosition = transform.position;
+
+        // Calculate the new position of the object based on the horizontal input and movement speed
+        float newXPos = currentPosition.x + (horizontalInput * movementSpeed * Time.deltaTime);
+
+        // Clamp the x position based on maxDistance
+        newXPos = Mathf.Clamp(newXPos, -maxDistance, maxDistance);
+
+        // Set the position of the object
+        transform.position = new Vector3(newXPos, currentPosition.y, currentPosition.z);
+
+
+        ///-----Rotation of the Pod -------------
+
+        float xVelocity = rb.velocity.x;
+
+        // Calculate the target rotation based on the x velocity
+        float targetRotationX = Mathf.Lerp(-maxRotation, maxRotation, (xVelocity + 5f) / 10f); // Maps xVelocity from range [-5, 5] to range [0, 1]
+
+        // Get the current rotation of the object
+        Quaternion currentRotation = transform.rotation;
+
+        // Set the target rotation around the x axis
+        Quaternion targetRotation = Quaternion.Euler(targetRotationX, currentRotation.eulerAngles.y, currentRotation.eulerAngles.z);
+
+        // Rotate the object towards the target rotation using the rotate function
+        transform.rotation = Quaternion.RotateTowards(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
 
     }
 }
